b960b2ce0671f957fca9d7e7a69a7ba37c2aa4e0
Yurii Makukha <yura@consulting.mk>
2020-10-13 16:23:06 +0300

    change country related data

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,9 @@
 FILE=config_backend.js
 if [ -f $FILE ]; then
     cd ..
-    git clone https://github.com/opentender-ug/opentender-data.git data
-    git clone https://github.com/opentender-ug/opentender-backend.git backend
-    git clone https://github.com/opentender-ug/opentender-frontend.git frontend
+    git clone https://github.com/opentender-ug-health/opentender-data.git data
+    git clone https://github.com/opentender-ug-health/opentender-backend.git backend
+    git clone https://github.com/opentender-ug-health/opentender-frontend.git frontend
     cp setup/config_backend.js backend/config.js
     cp setup/config_frontend.js frontend/config.js
     cd backend
