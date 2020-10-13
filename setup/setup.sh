9a513a57de7bc723c320d06accd536c8549480f9
Yurii Makukha <yura@consulting.mk>
2020-10-13 12:23:03 +0300

    change repository links

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,9 @@
 FILE=config_backend.js
 if [ -f $FILE ]; then
     cd ..
-    git clone https://github.com/opentender-jm/opentender-data.git data
-    git clone https://github.com/opentender-jm/opentender-backend.git backend
-    git clone https://github.com/opentender-jm/opentender-frontend.git frontend
+    git clone https://github.com/opentender-ug/opentender-data.git data
+    git clone https://github.com/opentender-ug/opentender-backend.git backend
+    git clone https://github.com/opentender-ug/opentender-frontend.git frontend
     cp setup/config_backend.js backend/config.js
     cp setup/config_frontend.js frontend/config.js
     cd backend
